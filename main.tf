cfaccf7c22cf8e469091a5509eac2a1c6c5b6bd0
Elena Shumanova <elena.shumanova@hotmail.com>
2021-11-03 16:06:00 +0000

    Fixed our pipeline

--- main.tf
+++ main.tf
@@ -12,6 +12,11 @@ terraform {
     }
 }
 
+variable "imagebuild" {
+  type        = string
+  description = "Latest image build"
+}
+
 
 resource "azurerm_resource_group" "tf_test" {
     name = "tfmainrg"
@@ -30,7 +35,7 @@ resource "azurerm_container_group" "tfcg_test" {
 
   container {
       name            = "weatherapi"
-      image           = "charamia29/weatherapi"
+      image           = "charamia29/weatherapi:${var.imagebuild}"
         cpu             = "1"
         memory          = "1"
 
